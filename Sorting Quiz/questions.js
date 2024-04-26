8d8cac10693f0e5da038008ff216933553df1473
wizardzworld <167012458+wizardzworld@users.noreply.github.com>
2024-04-26 19:56:00 +0530

    Update questions.js

--- Sorting Quiz/questions.js
+++ Sorting Quiz/questions.js
@@ -2,102 +2,102 @@ const questions = [
     {
         question: "You witness a group of bullies picking on a younger student. What do you do?",
         options: [
-            "Step in immediately to defend the younger student",
-            "Find a clever way to outmaneuver the bullies and protect the student",
             "Comfort the younger student and offer them support",
-            "Gather evidence of the bullying and report it to the appropriate authority"
+            "Step in immediately to defend the younger student",
+            "Gather evidence of the bullying and report it to the appropriate authority",
+            "Find a clever way to outmaneuver the bullies and protect the student"
         ],
-        answers: ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
+        answers: ["Hufflepuff", "Gryffindor", "Ravenclaw", "Slytherin"]
     },
     {
         question: "You find out that a friend has been cheating on a test. How do you react?",
         options: [
             "Confront your friend directly and urge them to come clean",
+            "Consider the ethical implications and advise your friend to make things right",
             "Help your friend cover up the cheating to avoid getting caught",
-            "Talk to your friend privately and encourage them to confess and accept the consequences",
-            "Consider the ethical implications and advise your friend to make things right"
+            "Talk to your friend privately and encourage them to confess and accept the consequences"
         ],
-        answers: ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
+        answers: ["Gryffindor", "Ravenclaw", "Slytherin", "Hufflepuff"]
     },
     {
         question: "You're assigned a group project with classmates who aren't pulling their weight. How do you handle it?",
         options: [
+            "Analyze the situation and come up with a strategy to motivate your teammates and improve collaboration",
             "Take charge and delegate tasks to ensure the project gets done on time",
-            "Manipulate the situation to ensure you get credit for the project's success",
             "Support your teammates and offer to help them with their tasks",
-            "Analyze the situation and come up with a strategy to motivate your teammates and improve collaboration"
+            "Manipulate the situation to ensure you get credit for the project's success"
         ],
-        answers: ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
+        answers: ["Ravenclaw", "Gryffindor", "Hufflepuff", "Slytherin"]
     },
     {
         question: "You come across a mysterious object with potentially magical properties. What's your first instinct?",
         options: [
+            "Research the object's history and properties before deciding how to proceed",
             "Touch or interact with the object to see what happens",
-            "Keep the object to yourself and try to unlock its secrets",
             "Seek advice from trusted friends or mentors before doing anything",
-            "Research the object's history and properties before deciding how to proceed"
+            "Keep the object to yourself and try to unlock its secrets"
         ],
-        answers: ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
+        answers: ["Ravenclaw", "Gryffindor", "Hufflepuff", "Slytherin"]
     },
     {
         question: "How do you typically organize your schedule and prioritize your tasks?",
         options: [
-            "I prefer to tackle tasks as they come up, often diving headfirst into whatever seems most urgent or exciting at the moment",
             "I strategically plan out my schedule, focusing on achieving my long-term goals and maximizing efficiency",
-            "I prioritize maintaining a balance between my personal life and responsibilities, making sure to allocate time for both work and relaxation",
-            "I enjoy exploring different methods of time management and organization, constantly seeking ways to optimize productivity and creativity"
+            "I prefer to tackle tasks as they come up, often diving headfirst into whatever seems most urgent or exciting at the moment",
+            "I enjoy exploring different methods of time management and organization, constantly seeking ways to optimize productivity and creativity",
+            "I prioritize maintaining a balance between my personal life and responsibilities, making sure to allocate time for both work and relaxation"
         ],
-        answers: ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
+        answers: ["Ravenclaw", "Gryffindor", "Slytherin", "Hufflepuff"]
     },
     {
         question: "How do you prefer to spend your weekends?",
         options: [
             "Engaging in adventurous activities or sports",
-            "Attending social events or networking",
             "Relaxing at home with loved ones or close friends",
-            "Exploring museums, libraries, or new intellectual pursuits"
+            "Exploring museums, libraries, or new intellectual pursuits",
+            "Attending social events or networking"
         ],
-        answers: ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
+        answers: ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
     },
     {
         question: "What type of setting would you choose for a dream vacation?",
         options: [
-            "A rugged wilderness or exotic location for outdoor exploration",
-            "A luxurious destination with exclusive experiences",
             "A cozy retreat by the beach or in the mountains",
-            "A culturally rich city known for its history and art"
+            "A rugged wilderness or exotic location for outdoor exploration",
+            "A culturally rich city known for its history and art",
+            "A luxurious destination with exclusive experiences"
         ],
-        answers: ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
+        answers: ["Hufflepuff", "Gryffindor", "Ravenclaw", "Slytherin"]
     },
     {
         question: "How do you handle unexpected changes or challenges in your life?",
         options: [
             "Embrace them with enthusiasm and adapt as needed",
             "Strategize and find new opportunities within the changes",
-            "Lean on friends and family for support during difficult times",
-            "Analyze the situation and learn from the experience"
+            "Analyze the situation and learn from the experience",
+            "Lean on friends and family for support during difficult times"
         ],
-        answers: ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
+        answers: ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"]
     },
     {
         question: "Which of these hobbies would you be most likely to pursue?",
         options: [
-            "Rock climbing or skydiving",
             "Chess or strategic games",
-            "Gardening or cooking",
-            "Writing or painting"
+            "Rock climbing or skydiving",
+            "Writing or painting",
+            "Gardening or cooking"
         ],
-        answers: ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
+        answers: ["Ravenclaw", "Gryffindor", "Slytherin", "Hufflepuff"]
     },
     {
         question: "What motivates you to excel in your endeavors?",
         options: [
+            "Curiosity and thirst for knowledge and understanding",
             "Desire for personal achievement and glory",
             "Ambition to rise above others and achieve recognition",
-            "Dedication to making a positive impact on those around you",
-            "Curiosity and thirst for knowledge and understanding"
+            "Dedication to making a positive impact on those around you"
         ],
-        answers: ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
+        answers: ["Ravenclaw", "Gryffindor", "Slytherin", "Hufflepuff"]
     }
 ];
 
