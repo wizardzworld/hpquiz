586a8962a2e2eb77c5dd3a49f7729fc7a00a36ef
wizardzworld <167012458+wizardzworld@users.noreply.github.com>
2024-04-26 19:12:20 +0530

    Add files via upload

---
+++ Sorting Quiz/questions.js
@@ -0,0 +1,110 @@
+const questions = [
+    {
+        question: "You witness a group of bullies picking on a younger student. What do you do?",
+        options: [
+            "Step in immediately to defend the younger student",
+            "Find a clever way to outmaneuver the bullies and protect the student",
+            "Comfort the younger student and offer them support",
+            "Gather evidence of the bullying and report it to the appropriate authority"
+        ],
+        answers: ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
+    },
+    {
+        question: "You find out that a friend has been cheating on a test. How do you react?",
+        options: [
+            "Confront your friend directly and urge them to come clean",
+            "Help your friend cover up the cheating to avoid getting caught",
+            "Talk to your friend privately and encourage them to confess and accept the consequences",
+            "Consider the ethical implications and advise your friend to make things right"
+        ],
+        answers: ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
+    },
+    {
+        question: "You're assigned a group project with classmates who aren't pulling their weight. How do you handle it?",
+        options: [
+            "Take charge and delegate tasks to ensure the project gets done on time",
+            "Manipulate the situation to ensure you get credit for the project's success",
+            "Support your teammates and offer to help them with their tasks",
+            "Analyze the situation and come up with a strategy to motivate your teammates and improve collaboration"
+        ],
+        answers: ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
+    },
+    {
+        question: "You come across a mysterious object with potentially magical properties. What's your first instinct?",
+        options: [
+            "Touch or interact with the object to see what happens",
+            "Keep the object to yourself and try to unlock its secrets",
+            "Seek advice from trusted friends or mentors before doing anything",
+            "Research the object's history and properties before deciding how to proceed"
+        ],
+        answers: ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
+    },
+    {
+        question: "How do you typically organize your schedule and prioritize your tasks?",
+        options: [
+            "I prefer to tackle tasks as they come up, often diving headfirst into whatever seems most urgent or exciting at the moment",
+            "I strategically plan out my schedule, focusing on achieving my long-term goals and maximizing efficiency",
+            "I prioritize maintaining a balance between my personal life and responsibilities, making sure to allocate time for both work and relaxation",
+            "I enjoy exploring different methods of time management and organization, constantly seeking ways to optimize productivity and creativity"
+        ],
+        answers: ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
+    },
+    {
+        question: "How do you prefer to spend your weekends?",
+        options: [
+            "Engaging in adventurous activities or sports",
+            "Attending social events or networking",
+            "Relaxing at home with loved ones or close friends",
+            "Exploring museums, libraries, or new intellectual pursuits"
+        ],
+        answers: ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
+    },
+    {
+        question: "What type of setting would you choose for a dream vacation?",
+        options: [
+            "A rugged wilderness or exotic location for outdoor exploration",
+            "A luxurious destination with exclusive experiences",
+            "A cozy retreat by the beach or in the mountains",
+            "A culturally rich city known for its history and art"
+        ],
+        answers: ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
+    },
+    {
+        question: "How do you handle unexpected changes or challenges in your life?",
+        options: [
+            "Embrace them with enthusiasm and adapt as needed",
+            "Strategize and find new opportunities within the changes",
+            "Lean on friends and family for support during difficult times",
+            "Analyze the situation and learn from the experience"
+        ],
+        answers: ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
+    },
+    {
+        question: "Which of these hobbies would you be most likely to pursue?",
+        options: [
+            "Rock climbing or skydiving",
+            "Chess or strategic games",
+            "Gardening or cooking",
+            "Writing or painting"
+        ],
+        answers: ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
+    },
+    {
+        question: "What motivates you to excel in your endeavors?",
+        options: [
+            "Desire for personal achievement and glory",
+            "Ambition to rise above others and achieve recognition",
+            "Dedication to making a positive impact on those around you",
+            "Curiosity and thirst for knowledge and understanding"
+        ],
+        answers: ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
+    }
+];
+
+const houseImages = {
+        Gryffindor: "https://i.pinimg.com/originals/7b/29/5e/7b295ec71ad8886880e560774486fe39.jpg",
+        Slytherin: "https://i.pinimg.com/564x/37/d7/3d/37d73d2ac0a3cb2df4f1697def4a5053.jpg",
+        Hufflepuff: "https://i.pinimg.com/564x/34/27/bd/3427bd5072cf5552bdd09ef5e64620cf.jpg",
+        Ravenclaw: "https://i.pinimg.com/564x/00/4a/1c/004a1c19a8cf6fb41bb3bddd6caef647.jpg"
+    };
+
